edef22e05e04df090860289f79bbeada9684c41b
Sergey-Shchipanov <to.s.shchipanov@gmail.com>
2020-12-15 09:53:57 +0200

    init

---
+++ src/main/java/com/shchipanov/bluerbn/BlueRibbonTestTaskApplication.java
@@ -0,0 +1,13 @@
+package com.shchipanov.bluerbn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlueRibbonTestTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlueRibbonTestTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shchipanov/bluerbn/BlueRibbonTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shchipanov.bluerbn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlueRibbonTestTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
